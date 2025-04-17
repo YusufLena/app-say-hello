6dd9bbd3a064654856ac5a1467348750c19993f9
YusufLena <sealtilena12@gmail.com>
2025-04-17 19:23:24 +0700

    second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gituhub.com/YusufLena/app-say-hello
 
 go 1.24.1
+
+require github.com/YusufLena/golang-say-hello v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/YusufLena/golang-say-hello"
+)
+func main() {
+	fmt.Println(go_say_hello.SayHello(" Sealti"))
+}
