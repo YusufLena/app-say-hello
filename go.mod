b4e11ca74d457c8ad12790630e59372a61f5cc20
YusufLena <sealtilena12@gmail.com>
2025-04-18 19:59:34 +0700

    update modules from golang_say_hello

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module gituhub.com/YusufLena/app-say-hello
 
 go 1.24.1
 
-require github.com/YusufLena/golang-say-hello v1.0.1 // indirect
+require github.com/YusufLena/golang-say-hello/v3 v3.0.4 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	go_say_hello "github.com/YusufLena/golang-say-hello"
+	go_say_hello "github.com/YusufLena/golang-say-hello/v3"
 )
 func main() {
-	fmt.Println(go_say_hello.SayHello(" Sealti"))
+	fmt.Println(go_say_hello.SayHello("Anjing", 17))
 }
